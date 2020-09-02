babfc9054c783e689b7f4de1419b6c42329c551f
mohuneko <b0wdmohu@gmail.com>
2020-09-02 20:56:26 +0900

    add

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+//canvas
 'use strict';
 
 {
@@ -23,10 +25,19 @@
     // ctx.strokeRect(70, 70, 50, 50);
 
     // ctx.createLinearGradient(x0, y0, x1, y1);
-    const g = ctx.createLinearGradient(0, 0, canvas.width, 0);
+    // const g = ctx.createLinearGradient(0, 0, canvas.width, 0);
+
+    // const g = ctx.createRadialGradient(
+    //   x0, y0, r0,
+    //   x1, y1, r1
+    // );
+    const g = ctx.createRadialGradient(
+      canvas.width / 2, canvas.height / 2, 50, 
+      canvas.width / 2 + 100, canvas.height /2 + 200, 500
+    );
 
     g.addColorStop(0, '#f00');
-    g.addColorStop(0.3, '#0f0');
+    g.addColorStop(0.1, '#0f0');
     g.addColorStop(1, '#00f');
 
     ctx.fillStyle = g;
